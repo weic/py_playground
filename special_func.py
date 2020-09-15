5f7cf33d3815f4bad514fd446f12dd4cd0a8301b
Wei C <weic.github@gmail.com>
2020-09-14 23:05:09 -0400

    Updating special function file.

--- special_func.py
+++ special_func.py
@@ -58,3 +58,12 @@ def func_compute(n):
  return lambda x : x * n
 result = func_compute(2)
 print(result(3))
+
+# 3. Write a Python program to sort a list of tuples using Lambda.
+subject_marks = [('English', 88), ('Science', 90), ('Maths', 97), ('Social sciences', 82)]
+print("Original list of tuples:")
+print(subject_marks)
+subject_marks.sort(key = lambda x: x[1])
+print("\nSorting the List of Tuples:")
+print(subject_marks)
+
